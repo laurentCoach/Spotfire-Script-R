c7dcf5e3c383ee1022792621d238895d1f355316
laurentCoach <laurentcesaro17@gmail.com>
2017-05-16 13:44:32 +0200

    Count Row Marked in table

---
+++ CountRowMarked.R
@@ -0,0 +1,10 @@
+#Laurent Cesaro
+#Date 17/03/2017
+
+#Compte le nombre de lot marked
+Count<-number
+
+CountPP<-sum(table$var=="")
+CountPF<-sum(table$var=="")
+CountFP<-sum(table$var=="")
+CountFF<-sum(table$var=="")
